318dc608bcdb96038ab8b7f2aadaaf50c6de0b80
Rvjonh <jonhvelasco3@gmail.com>
2023-02-06 12:01:41 -0400

    blog section description updated

--- src/Routes/Blog/index.js
+++ src/Routes/Blog/index.js
@@ -7,16 +7,16 @@ import Button from './../../Components/Button';
 
 import imgSoon from './../../Assets/Extra/coming-soon.jpg';
 
-export default function Blog(){
-    return(
+export default function Blog() {
+    return (
         <MainWrap>
             <BlogSections />
         </MainWrap>
     )
 }
 
-export function BlogSections(){
-    return(
+export function BlogSections() {
+    return (
         <section>
 
             <div className='title-header'>
@@ -24,32 +24,30 @@ export function BlogSections(){
             </div>
 
             <p className="text text-blog">
-                In this part you can take a look at some of my projects, 
-                I have put them a little description and if you like any 
-                of them I would like you to send me a message with your 
-                thoughts. I look forward to hearing from you.
+                In this part you can take a look at some of my blog posts,
+                my familiar language is spanish, so I use these articles to exprese me and explain what a I have learn from many aspect in my career. let me a comment to get meet 😉.
             </p>
-            
+
             <div className='posts-container'>
-                <BlogPost img={imgSoon} 
-                            text="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic"
-                            />
-                <BlogPost img={imgSoon} 
-                            text="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic"
-                            />
-                <BlogPost img={imgSoon} 
-                            text="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic"
-                            />
-                <BlogPost img={imgSoon} 
-                            text="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic"
-                            />
+                <BlogPost img={imgSoon}
+                    text="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic"
+                />
+                <BlogPost img={imgSoon}
+                    text="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic"
+                />
+                <BlogPost img={imgSoon}
+                    text="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic"
+                />
+                <BlogPost img={imgSoon}
+                    text="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic"
+                />
             </div>
 
             <div className='links-controls'>
                 <Link to='/services'>
                     <Button textColor='#ffffff'
-                            bgColor='rgb(0, 127, 255)'>
-                                View More
+                        bgColor='rgb(0, 127, 255)'>
+                        View More
                     </Button>
                 </Link>
             </div>
